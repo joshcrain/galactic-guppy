dc94fc0fe7c6c9bda6927afacff72172fd3e751f
joshcrain <josh.crain@gmail.com>
2023-05-11 09:46:20 -0500

    pass through css

--- .eleventy.js
+++ .eleventy.js
@@ -3,5 +3,5 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("posts/*/*.jpg");
-  eleventyConfig.addPassthroughCopy("testcss/*/*.css");
+  eleventyConfig.addPassthroughCopy("testcss/*.css");
 };
