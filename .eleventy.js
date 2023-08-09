50cdd78079f345a340cc9d883445ecb25e152551
joshcrain <josh.crain@gmail.com>
2023-08-09 10:47:11 -0500

    test eleventy image

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,27 @@
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
-  eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("posts/*/*.jpg");
   eleventyConfig.addPassthroughCopy("testcss/*.css");
+
+
+  eleventyConfig.addShortcode("image", async function(src, alt, sizes) {
+		let metadata = await Image(src, {
+			widths: [300, 600],
+			formats: ["avif", "jpeg"],
+      urlPath: "/img/",
+      outputDir: "./_site/img/"
+		});
+
+		let imageAttributes = {
+			alt,
+			sizes,
+			loading: "lazy",
+			decoding: "async",
+		};
+
+		// You bet we throw an error on a missing alt (alt="" works okay)
+		return Image.generateHTML(metadata, imageAttributes);
+	});
 };
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Page header
+# The Galactic Guppy
+
+The Galactic Guppy is a poorly drawn comic by [Josh Crain](https://joshcrain.io/). This is the website.
